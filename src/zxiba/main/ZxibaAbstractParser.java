d45456ed07b3ba3fc4dbfe8bae34f9f3252b833e
LowDensity <hailhailHaily@hotmail.com>
2021-02-11 02:19:16 +0800

    Simplify structure,
    all parsers except ZxibaParser  supports only one keyword / option

--- src/zxiba/main/ZxibaAbstractParser.java
+++ src/zxiba/main/ZxibaAbstractParser.java
@@ -8,7 +8,9 @@ import java.util.List;
 public abstract class ZxibaAbstractParser {
 	protected List<String> allParams;
 	protected HashMap<String,int[]> startEnds;// {keywordName =>[startIndex , endIndex] ， 將結果儲存起來，接下來有需要的時候直接使用
-	
+	private boolean containsKeyName = false;
+	private String keyName;
+	private int start,end;
 	protected abstract String addPrefix(String keyName);	//將沒有任何前綴的名稱format成符合當下需求的樣子，如 abc => -opt 或 --opt
 	protected abstract boolean isKey(String param); //是否是當下認定的 pattern key
 	
@@ -16,18 +18,40 @@ public abstract class ZxibaAbstractParser {
 	
 	public abstract String[] getParamValues(String patternKey,int valueCount,int range);//參數數量必須 介於value_count 跟 value_count + range，數量不合時回傳null
 
-	
-	public ZxibaAbstractParser(String[] args) {
-		this(Arrays.asList(args)); 
+	public ZxibaAbstractParser(String[] args ,String keyName) {
+		this(Arrays.asList(args),keyName);
 	}
 	
-	public ZxibaAbstractParser(List<String> args) {
+	public ZxibaAbstractParser(List<String> args,String keyName) {
+		this.keyName=keyName;
 		allParams = args;
-		startEnds=new HashMap<String,int[]>();
+		int[] startEnd = getStartEnd(keyName);
+		if(startEnd[0]== -1 ) {return ;}
+		start = startEnd[0];
+		end = startEnd[1];
 	}
 	
-	public ZxibaAbstractParser(List<String> args,String... parseKeyNames) {
-		this(args);
+	
+	public boolean hasValueCount(int optionCount , int range) {
+		return false;
+	}
+	
+	//直接取值
+	public String[] getValues() {
+		if(!containsKeyName) {return new String[0];}
+		//去掉頭
+		return allParams.subList(start+1, end).toArray(new String[end-start-1]);
+	}
+	
+	//取值同時檢查數量
+	public String[] getValues(int valueCount,int range) {
+		String[] values= this.getValues();
+		range = range < 0 ? range *-1 :range;
+		//如果數量不對直接log並丟出Exception
+		if(values.length > valueCount+range || values.length < valueCount-range) {
+			System.err.println("keyname does not contain required amount of parameters");
+		}
+		return values;
 	}
 	
 	
@@ -40,58 +64,7 @@ public abstract class ZxibaAbstractParser {
 	protected boolean isSeparator(String param) {
 		return param.trim().equals("--");
 	};
-	//是否有此option存在，不理會參數數量限制
-	public boolean hasKey(String keyName) {
-		return hasKeyWithParam(keyName,-1,0);
-	}
-	
-	
-	 /* 檢查是否帶有指定keyword以及 value_count ~value_count + range 數量的 value，value_count
-	 * value_count 與 range 均為0的時候表示此keyword 不接受任何參數
-	 * 主要用來處理0 - n 個參數的狀況
-	 * value_count 為 -1的時候表示參數數量沒有限制
-	 * 未傳入range時傳入之參數數量必須與value_count相同
-	 * value_count=-1 的時候沒有限制參數數量
-	 * 注意 value_count==-1 時 range 會被無視
-	 * 超出範圍時
-	 * */ 
-	public boolean hasKeyWithParam(String keyName,int valueCount,int range) {
-		int start,end,pValueCount;
-		
-		if(startEnds.containsKey(keyName)){return startEnds.get(keyName)!=null;}
-		start = getOptStart(addPrefix(keyName));
-		if(valueCount<0 ) {return start!=-1 ;}
-		end= getOptEndIndex(start);
-		startEnds.put(keyName,null);
-		
-		if(valueCount==0 && range==0 && end!=start){
-			System.err.println(String.format("Incorrect number of parameters , require 0 , received %d", end-start));
-			return false;
-		}
-		
-		
-		if((pValueCount=end-start) != valueCount  && range==0 ) {
-			System.err.println(
-					String.format("Incorrect number of parameters , require %d , received %d"
-					, valueCount
-					, pValueCount));
-			return false;
-		}
-		
-		//負數的話先做一次轉換確保下面的邏輯是正確的
-		range = range < 0 ? range*-1 : range;
-		if( (valueCount-range) < pValueCount || pValueCount > (valueCount+range)) {
-			System.err.println(
-					String.format("Incorrect number of parameters , maximum %d  minimum %d , received %d"
-							, valueCount-range
-							,valueCount+range
-							, pValueCount));
-			return false;
-		}
-		startEnds.put(keyName, new int[] {start,end});
-		return true;
-		
-	}
+
 	
 	
 	protected int[] getStartEnd(String patternKey) {
@@ -101,16 +74,16 @@ public abstract class ZxibaAbstractParser {
 		}else
 		{
 			startEnd=new int[] {
-					getOptStart(addPrefix(patternKey))
-					,getEndIndex(addPrefix(patternKey))
+					getOptStartIndex(addPrefix(patternKey))
+					,getOptEndIndex(addPrefix(patternKey))
 					};	
 		}
 		return startEnd;
 	}
 	
 		
-	protected int getEndIndex(String keyName) {
-		return getOptEndIndex(getOptStart(keyName));
+	protected int getOptEndIndex(String keyName) {
+		return getOptEndIndex(getOptStartIndex(keyName));
 	}
 	
 	protected int getOptEndIndex(int startIndex) {
@@ -120,7 +93,7 @@ public abstract class ZxibaAbstractParser {
 		return endIndex;
 	}
 	
-	protected int getOptStart(String keyName) {
+	protected int getOptStartIndex(String keyName) {
 		return allParams.indexOf(keyName);
 	}
 	
--- src/zxiba/main/ZxibaParser.java
+++ src/zxiba/main/ZxibaParser.java
@@ -14,10 +14,66 @@ import zxiba.exceptions.ParameterCountNotInRangeException;
  * */
 public class ZxibaParser extends ZxibaKeywordParser{
 
+	private HashMap<String,ZxibaKeywordParser> keywords=new HashMap<String,ZxibaKeywordParser>();
+	
 	public ZxibaParser(String[] args) {
 		super(args);
-		// TODO Auto-generated constructor stub
 	}
+	
+	//是否有此option存在，不理會參數數量限制
+	public boolean hasKey(String keyName) {
+		return hasKeyWithParam(keyName,-1,0);
+	}
+	
+	
+	 /* 檢查是否帶有指定keyword以及 value_count ~value_count + range 數量的 value，value_count
+	 * value_count 與 range 均為0的時候表示此keyword 不接受任何參數
+	 * 主要用來處理0 - n 個參數的狀況
+	 * value_count 為 -1的時候表示參數數量沒有限制
+	 * 未傳入range時傳入之參數數量必須與value_count相同
+	 * value_count=-1 的時候沒有限制參數數量
+	 * 注意 value_count==-1 時 range 會被無視
+	 * 超出範圍時
+	 * */ 
+	public boolean hasKeyWithParam(String keyName,int valueCount,int range) {
+		int start,end,pValueCount;
+		
+		if(startEnds.containsKey(keyName)){return startEnds.get(keyName)!=null;}
+		start = getOptStartIndex(addPrefix(keyName));
+		if(valueCount<0 ) {return start!=-1 ;}
+		end = getOptEndIndex(keyName);
+		startEnds.put(keyName,null);
+		
+		if(valueCount==0 && range==0 && end!=start){
+			System.err.println(String.format("Incorrect number of parameters , require 0 , received %d", end-start));
+			return false;
+		}
+		
+		
+		if((pValueCount=end-start) != valueCount  && range==0 ) {
+			System.err.println(
+					String.format("Incorrect number of parameters , require %d , received %d"
+					, valueCount
+					, pValueCount));
+			return false;
+		}
+		
+		//負數的話先做一次轉換確保下面的邏輯是正確的
+		range = range < 0 ? range*-1 : range;
+		if( (valueCount-range) < pValueCount || pValueCount > (valueCount+range)) {
+			System.err.println(
+					String.format("Incorrect number of parameters , maximum %d  minimum %d , received %d"
+							, valueCount-range
+							,valueCount+range
+							, pValueCount));
+			return false;
+		}
+		startEnds.put(keyName, new int[] {start,end});
+		return true;
+		
+	}
+
+	
 		
 
 	
